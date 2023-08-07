6b883b77dbe537ffd583a8135d822d2bd1ea9833
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2023-08-07 22:21:31 +0530

    Added salvo server endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,47 @@ mod dtos;
 mod models;
 mod utils;
 
-use std::sync::Arc;
-
+use dtos::SlotDataDto;
+use once_cell::sync::OnceCell;
+use salvo::prelude::*;
 use sqlx::SqlitePool;
+use std::sync::Arc;
 use tokio::sync::Mutex;
 use utils::scheduler;
 
+static SQLITE: OnceCell<SqlitePool> = OnceCell::new();
+
+#[inline]
+pub fn get_sqlite() -> &'static SqlitePool {
+    unsafe { SQLITE.get_unchecked() }
+}
+
 pub type AppResult<T> = Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+#[handler]
+async fn get_specific_slot(req: &mut Request, res: &mut Response) {
+    let slot_number = req.query::<i64>("slot").unwrap();
+    let data = sqlx::query_as!(
+        models::SlotData,
+        r#"
+            SELECT * 
+            FROM slot_data
+            WHERE slot = ?
+            "#,
+        slot_number
+    )
+    .fetch_optional(get_sqlite())
+    .await
+    .unwrap();
+
+    let dto: SlotDataDto = data.unwrap().into();
+
+    res.render(serde_json::to_string(&dto).unwrap())
+}
 
 #[tokio::main]
 async fn main() -> AppResult<()> {
+    tracing_subscriber::fmt().init();
     let db_url = env!("DATABASE_URL");
     let pool = SqlitePool::connect(db_url).await?;
     let db_pool = Arc::new(Mutex::new(pool));
@@ -36,6 +67,11 @@ async fn main() -> AppResult<()> {
     // println!("Starting the scheduler for updating the unexecuted slot");
     // let task3 =
     //     tokio::spawn(async move { scheduler::update_unexecuted_slot(db_pool_thread_3) }).await?;
+
+    let router = Router::with_path("slot").get(get_specific_slot);
+    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+
+    Server::new(acceptor).serve(router).await;
 
     let (result1, result2) = tokio::join!(task1, task2);
 
